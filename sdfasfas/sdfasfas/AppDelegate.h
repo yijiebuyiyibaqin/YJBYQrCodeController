171aec20ed04159153077df4088fce51a6aa1d60
your_name <your_email>
2017-03-06 09:49:25 +0800

    测试

---
+++ sdfasfas/sdfasfas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sdfasfas
+//
+//  Created by apple　　　 on 2017/3/6.
+//  Copyright © 2017年 yangguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
